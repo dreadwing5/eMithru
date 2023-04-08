a58459c35585ea4730ff363023143fabca6964d9
dreadwing5 <sachinspyk@outlook.com>
2023-04-08 16:27:17 +0530

    Added files

--- src/components/hook-form/index.js
+++ src/components/hook-form/index.js
@@ -1,10 +1,9 @@
-export * from './RHFCheckbox';
-export * from './RHFUpload';
+export * from "./RHFCheckbox";
+export * from "./RHFUpload";
 
-export { default as FormProvider } from './FormProvider';
+export { default as FormProvider } from "./FormProvider";
 
-export { default as RHFSwitch } from './RHFSwitch';
-export { default as RHFSelect } from './RHFSelect';
-export { default as RHFEditor } from './RHFEditor';
-export { default as RHFTextField } from './RHFTextField';
-export { default as RHFRadioGroup } from './RHFRadioGroup';
+export { default as RHFSwitch } from "./RHFSwitch";
+export { default as RHFSelect } from "./RHFSelect";
+export { default as RHFTextField } from "./RHFTextField";
+export { default as RHFRadioGroup } from "./RHFRadioGroup";
