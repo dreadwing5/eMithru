5dd249a1ea1fc5dd8e8a32d76951aa3c176e6942
dreadwing5 <sachinspyk@outlook.com>
2023-04-23 14:19:16 +0530

    improve login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import logo from "../public/logo.png";
 import {
   Box,
   Card,
@@ -12,14 +13,20 @@ import {
   Grid,
   Stack,
   Link,
+  Avatar,
+  useTheme,
 } from "@mui/material";
 import { useContext, useRef } from "react";
 import { loginCall } from "../apiCalls";
 import { AuthContext } from "../context/AuthContext";
+import Image from "mui-image";
+
+import Illustration from "../public/login_illustration.png";
 
 const Login = () => {
   const email = useRef();
   const password = useRef();
+  const theme = useTheme();
   const { isFetching, dispatch } = useContext(AuthContext);
 
   const handleSubmit = (e) => {
@@ -32,119 +39,139 @@ const Login = () => {
 
   return (
     <Container maxWidth="lg">
-      <Box
+      <Avatar
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          minHeight: "100vh",
-          justifyContent: "center",
+          background: theme.palette.background.paper,
+          width: 90,
+          height: 90,
+          margin: "0 auto",
+          mt: 2,
+          mb: 2,
         }}
       >
-        <Card
-          sx={{
-            p: 4,
-            mt: 4,
-            width: "100%",
-            boxShadow:
-              "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)",
-            borderRadius: 2,
-            backgroundColor: "background.paper",
+        <img
+          src={logo}
+          alt="CMRIT Logo"
+          width="100"
+          style={{
+            transform: "scale(1.3)",
+            filter:
+              theme.palette.mode === "dark"
+                ? "invert(100%) hue-rotate(180deg)"
+                : "none",
           }}
-        >
-          <Grid container spacing={8}>
-            <Grid item xs={12} md={6}>
-              <Box
-                sx={{
-                  ml: 3,
-                }}
-              >
-                <Box sx={{ mb: 7 }}>
-                  <Typography variant="h3" gutterBottom>
-                    Sign in to e-mithru
-                  </Typography>
-                  <Typography gutterBottom sx={{ color: "text.secondary" }}>
-                    Welcome back! Please Login to your account.
-                  </Typography>
-                </Box>
-                <Box>
-                  <Stack spacing={2} sx={{ textAlign: "left" }}>
-                    <Typography variant="subtitle1">Email Address</Typography>
-                    <TextField
-                      margin="normal"
-                      required
-                      fullWidth
-                      id="email"
-                      name="email"
-                      autoComplete="email"
-                      autoFocus
-                      inputRef={email}
-                    />
-                    <Typography variant="subtitle1">Password</Typography>
-                    <TextField
-                      margin="normal"
-                      required
-                      fullWidth
-                      name="password"
-                      type="password"
-                      id="password"
-                      autoComplete="current-password"
-                      inputRef={password}
-                    />
-                    <Box>
-                      <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Keep me logged in"
-                      />
-                    </Box>
+        />
+      </Avatar>
 
-                    <Button
-                      type="submit"
-                      fullWidth
-                      variant="contained"
-                      sx={{ mt: 3, mb: 2, borderRadius: 1, p: 2 }}
-                      disabled={isFetching}
+      <Card
+        sx={{
+          p: 4,
+          width: "100%",
+          boxShadow:
+            "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)",
+          borderRadius: 2,
+          backgroundColor: "background.paper",
+          boxShadow: " 0px 0px 20px rgba(0, 0, 0, 0.5)",
+        }}
+      >
+        <Grid container spacing={8}>
+          <Grid item xs={12} md={6}>
+            <Box
+              sx={{
+                ml: 3,
+                postion: "relative",
+              }}
+            >
+              <Box sx={{ mb: 7 }}>
+                <Typography variant="h3" gutterBottom>
+                  Sign in to e-mithru
+                </Typography>
+
+                <Typography gutterBottom sx={{ color: "text.secondary" }}>
+                  Welcome back! Please Login to your account.
+                </Typography>
+              </Box>
+              <Box>
+                <Stack spacing={2} sx={{ textAlign: "left" }}>
+                  <Typography variant="subtitle1">Email Address</Typography>
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    inputRef={email}
+                  />
+                  <Typography variant="subtitle1">Password</Typography>
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    inputRef={password}
+                  />
+                  <Box>
+                    <FormControlLabel
+                      control={<Checkbox value="remember" color="primary" />}
+                      label="Keep me logged in"
+                    />
+                  </Box>
+
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2, borderRadius: 1, p: 2 }}
+                    disabled={isFetching}
+                  >
+                    {isFetching ? (
+                      <CircularProgress size="20px" sx={{ color: "#fff" }} />
+                    ) : (
+                      "Log In"
+                    )}
+                  </Button>
+                  <Box sx={{ display: "flex" }}>
+                    <Link
+                      component="button"
+                      variant="body1"
+                      onClick={() => {
+                        // handle forgot password logic here
+                      }}
                     >
-                      {isFetching ? (
-                        <CircularProgress size="20px" sx={{ color: "#fff" }} />
-                      ) : (
-                        "Log In"
-                      )}
-                    </Button>
-                    <Box sx={{ display: "flex" }}>
-                      <Link
-                        component="button"
-                        variant="body1"
-                        onClick={() => {
-                          // handle forgot password logic here
-                        }}
-                      >
-                        Forgot password?
-                      </Link>
-                    </Box>
-                  </Stack>
-                </Box>
+                      Forgot password?
+                    </Link>
+                  </Box>
+                </Stack>
               </Box>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "flex-end",
-                  alignItems: "flex-end",
-                  minHeight: 600,
-                }}
-              >
-                <img
-                  alt="Illustration"
-                  src="https://via.placeholder.com/500x600"
-                  sx={{ width: "100%" }}
-                />
-              </Box>
-            </Grid>
+            </Box>
           </Grid>
-        </Card>
-      </Box>
+          <Grid item xs={12} md={6}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "center",
+                height: "70vh",
+              }}
+            >
+              <Image
+                src={Illustration}
+                height="65vh"
+                sx={{
+                  width: "100%",
+                  objectFit: "cover",
+                }}
+                duration={1000}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+      </Card>
     </Container>
   );
 };
