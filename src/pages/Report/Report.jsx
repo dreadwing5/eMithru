beb62522c619fb5f13b8d5cdcbb6dd0965f9f172
Sahaj Kedia <64389239+sahajkedia@users.noreply.github.com>
2023-04-28 02:38:22 +0530

    Add Report Feature
    Co-authored-by: Sachin Kumar <sachinspyk@outlook.com>

---
+++ src/pages/Report/Report.jsx
@@ -0,0 +1,354 @@
+import { useState, useEffect, React } from "react";
+
+import {
+  Avatar,
+  Box,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Container,
+  Tooltip,
+  Dialog,
+  DialogContent,
+  DialogActions,
+  InputAdornment,
+  Grid,
+  IconButton,
+  MenuItem,
+  Button,
+  TextField,
+} from "@mui/material";
+import exportToExcel from "./ExportToExcel";
+import { Search } from "@mui/icons-material";
+import GetApp from "@mui/icons-material/GetApp";
+
+import Page from "../../components/Page";
+import api from "../../utils/axios"; // replace with your actual API path
+
+/* 
+TODO : The export to excel button should be on the right side
+
+On top of the card there should be a filter form 
+
+New Features for data filt:
+
+
+Add a search bar which should be able to display only the rows that contains the user
+add sort by open and close
+Add date fileter with from and to date
+add sort option for date
+Add filter for category
+
+*/
+
+/*
+
+Excel report :
+
+The header should be marked bold, with a sticky header and it should have a color,
+Remove the _id and _v field from the data,
+date should be an excel date format,
+Excel file name should be a timestamp
+participant name should be there as comma seperate list
+
+
+*/
+const Report = () => {
+  const [threads, setThreads] = useState([]);
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [fromDate, setFromDate] = useState("");
+  const [toDate, setToDate] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleFromDateChange = (event) => {
+    setFromDate(event.target.value);
+  };
+
+  const handleToDateChange = (event) => {
+    setToDate(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const filteredThreads = threads.filter((thread) => {
+    // Check for matching participant names
+    const hasMatchingParticipant = thread.participants.some((participant) =>
+      participant.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    // Check if the thread's category matches the selected category
+    const categoryMatches =
+      !selectedCategory || thread.category === selectedCategory;
+
+    // Check if the thread's open date is within the specified date range
+    const openDate = new Date(thread.openDate);
+    const fromDateObj = fromDate ? new Date(fromDate) : null;
+    const toDateObj = toDate ? new Date(toDate) : null;
+
+    const openDateInRange =
+      (!fromDateObj || openDate >= fromDateObj) &&
+      (!toDateObj || openDate <= toDateObj);
+
+    return hasMatchingParticipant && categoryMatches && openDateInRange;
+  });
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleOpenDialog = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setIsDialogOpen(false);
+  };
+
+  useEffect(() => {
+    const fetchThreads = async () => {
+      try {
+        const response = await api.get("threads");
+        if (response.status === 200) {
+          const { data } = response.data;
+          setThreads(data.threads);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchThreads();
+  }, []);
+
+  const statusColors = {
+    open: "#4caf50",
+    "In Progress": "#ff9800",
+    closed: "#f44336",
+  };
+
+  const handleExportToExcel = () => {
+    const data = threads;
+
+    // console.log(data);
+    const timestamp = new Date().toISOString();
+
+    exportToExcel(data, `${timestamp}-counselling-report.xlsx`);
+
+    // exportToExcel(data, "threads");
+  };
+
+  return (
+    <Page title="Thread">
+      <Container maxWidth="xl" sx={{ overflowX: "hidden", overflowY: "auto" }}>
+        <Box
+          display="flex"
+          sx={{ mt: 6 }}
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Typography variant="h4" component="h1">
+            Threads Report
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleExportToExcel}
+            startIcon={<GetApp />}
+          >
+            Export to Excel
+          </Button>
+        </Box>
+        <Grid container spacing={2} sx={{ mt: 2, mb: 3 }}>
+          <Grid item xs={6} md={2}>
+            <TextField
+              label="Sort by"
+              variant="outlined"
+              select
+              fullWidth
+              defaultValue=""
+            >
+              <MenuItem value="">None</MenuItem>
+              <MenuItem value="openDate">Open date</MenuItem>
+              <MenuItem value="closeDate">Close date</MenuItem>
+            </TextField>
+          </Grid>
+          <Grid item xs={6} md={2}>
+            <TextField
+              label="Category"
+              variant="outlined"
+              select
+              fullWidth
+              defaultValue=""
+            >
+              <MenuItem value="">All</MenuItem>
+              {/* Add your categories here */}
+              <MenuItem value="category1">Category 1</MenuItem>
+              <MenuItem value="category2">Category 2</MenuItem>
+            </TextField>
+          </Grid>
+          <Grid item xs={6} md={2}>
+            <TextField
+              label="From date"
+              type="date"
+              variant="outlined"
+              fullWidth
+              value={fromDate}
+              onChange={handleFromDateChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={6} md={2}>
+            <TextField
+              label="To date"
+              type="date"
+              variant="outlined"
+              fullWidth
+              value={toDate}
+              onChange={handleToDateChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <TextField
+              label="Search"
+              fullWidth
+              variant="outlined"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton>
+                      <Search />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+        </Grid>
+        <Paper sx={{ width: "100%", overflow: "hidden" }}>
+          <TableContainer>
+            <Table>
+              <TableHead sx={{ backgroundColor: "background.neutral" }}>
+                <TableRow>
+                  <TableCell>Title</TableCell>
+                  <TableCell>Description</TableCell>
+                  <TableCell>Status</TableCell>
+                  <TableCell>Category</TableCell>
+                  <TableCell>Open Date</TableCell>
+                  <TableCell>Close date</TableCell>
+                  <TableCell>Created by</TableCell>
+                  <TableCell>Members</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {threads.map((thread) => (
+                  <TableRow key={thread._id}>
+                    <TableCell>{thread.title}</TableCell>
+
+                    <TableCell>
+                      <Box
+                        sx={{
+                          maxHeight: "5rem",
+                          maxWidth: "20rem",
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                        }}
+                      >
+                        <Typography sx={{ textAlign: "justify" }}>
+                          Lorem, ipsum dolor sit amet consectetur adipisicing
+                          elit. Consequuntur adipisci, illo repellendus
+                          molestias consectetur obcaecati sapiente, et nemo
+                          laudantium dolore tempora soluta dolorum ea deleniti
+                          iste explicabo. Alias, sapiente asperiores.
+                        </Typography>
+                      </Box>
+                      <Button onClick={handleOpenDialog}>Read more</Button>
+                      <Dialog open={isDialogOpen} onClose={handleCloseDialog}>
+                        <DialogContent>
+                          <Typography>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing
+                            elit. Consequuntur adipisci, illo repellendus
+                            molestias consectetur obcaecati sapiente, et nemo
+                            laudantium dolore tempora soluta dolorum ea deleniti
+                            iste explicabo. Alias, sapiente asperiores.
+                          </Typography>
+                        </DialogContent>
+                        <DialogActions>
+                          <Button onClick={handleCloseDialog}>Close</Button>
+                        </DialogActions>
+                      </Dialog>
+                    </TableCell>
+                    <TableCell>
+                      <Typography
+                        variant="body2"
+                        style={{
+                          display: "inline-flex",
+                          alignItems: "center",
+                          backgroundColor: statusColors[thread.state],
+                          borderRadius: "12px",
+                          padding: "0 8px",
+                          color: "white",
+                        }}
+                      >
+                        {thread.state}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>{thread.tag}</TableCell>
+                    <TableCell>
+                      {new Date(thread.openDate).toLocaleDateString()}
+                    </TableCell>
+                    <TableCell>
+                      {thread.closeDate
+                        ? new Date(thread.closeDate).toLocaleDateString()
+                        : "N/A"}
+                    </TableCell>
+                    <TableCell>{thread.createdBy.name}</TableCell>
+                    <TableCell style={{ display: "flex", cursor: "pointer" }}>
+                      {thread.participants
+                        .slice(0, 3)
+                        .map((participant, idx) => (
+                          <Tooltip
+                            key={idx}
+                            title={`${participant.name}`}
+                            placement="top"
+                          >
+                            <Avatar
+                              sx={{
+                                ml: idx === 0 ? 0 : -1,
+                              }}
+                              alt={participant.name}
+                            >
+                              {participant.name[0]}
+                            </Avatar>
+                          </Tooltip>
+                        ))}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+      </Container>
+    </Page>
+  );
+};
+
+export default Report;
